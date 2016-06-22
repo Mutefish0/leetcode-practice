8cf6658a322311d051a56480897eebf16b4bab05
riddleoo <648262030@qq.com>
2016-06-22 17:38:18 +0800

    最大子数组问题的线性时间解法

---
+++ solutions/Maximum-Subarray.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let maxSubArray = function(nums) {
+    let length = nums.length
+    if(length===1) return nums[0]
+    let potential = []
+    potential[-1] = 0
+    for(i=0;i<length;i++)
+        potential[i] = potential[i-1] + nums[i]
+    let left = -1
+    let right = 0
+    let left_successor = -1
+    let left_successor_min = 0
+    for(i=0;i<length;i++){
+        if(potential[i]-left_successor_min>=potential[right]-potential[left]){
+            left = left_successor
+            right = i
+        }
+        else if(potential[i]>potential[right]) right = i 
+        if(potential[i]<left_successor_min) {
+            left_successor_min = potential[i]
+            left_successor = i
+        }
+    }
+    return potential[right]-potential[left]
+}
