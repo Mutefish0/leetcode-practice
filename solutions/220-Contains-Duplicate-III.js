1fb2fc7228af54c371ba25f4f8f4c0c2377d9216
riddleoo <648262030@qq.com>
2016-06-30 12:14:30 +0800

    ---

---
+++ solutions/220-Contains-Duplicate-III.js
@@ -0,0 +1,10 @@
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+    let length = nums.length 
+    let end
+    for(i=0;i<length;i++){
+        end = i + k
+        for(j=i+1;j<=end&&j<length;j++)
+            if(Math.abs(nums[i]-nums[j])<=t) return true
+    }
+    return false
+}
