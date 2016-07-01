d91e7f8260a2a593e91951e2f99c723c9fc511fe
riddleoo <648262030@qq.com>
2016-07-01 17:31:21 +0800

    --

--- util/index.js
+++ util/index.js
@@ -5,3 +5,7 @@ function Measurer(delegate){
     let endTime = (new Date()).getTime()
     console.log(`Prog finished,${endTime-startTime} ms passed`)
 }
+//打乱数组的顺序
+function randomize(array){
+    array.sort((a,b)=>Math.random()-0.5)
+}
