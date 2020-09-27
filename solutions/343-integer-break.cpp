84b24659f519bf2bd1932548d7ca4a7d480cd2eb
Mutefish0 <648262030@qq.com>
2020-09-27 23:26:48 +0800

    343-integer-break

---
+++ solutions/343-integer-break.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int integerBreak(int n) {
+    vector<int> dp(n + 1, 1);
+    dp[0] = 0;
+    dp[1] = 0;
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+      for (int j = 1; j < i; j++) {
+        dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
+      }
+    }
+    return dp[n];
+  }
+};
+
+int main() {
+  Solution s;
+  cout << s.integerBreak(10) << endl;
+  return 0;
+}
